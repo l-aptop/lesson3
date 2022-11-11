f3ed9397605ab27f1e2a69f5666d2767811342e9
l-aptop <farrada1337@gmail.com>
2022-11-11 22:58:53 +0200

    update

---
+++ 2.py
@@ -0,0 +1,21 @@
+import random
+
+
+class Hide:
+    __slots__ = "result"
+
+    def __init__(self, num: int, num2: int):
+        self.__hide(num, num2)
+
+    def __hide(self, num, num2):
+        self.result = eval(f"{num}{random.choice(('-', '+', '*', '/', '%', '**', '//'))}{num2}")
+
+    def __str__(self):
+        return str(self.result)
+
+    def __repr__(self):
+        return str(self)
+
+
+hidden = Hide(2, 4)
+print(hidden)
